d4ff57423b7242bf051033d349e684db39483dec
Brian Grubba <brian@snarebox.com>
2023-05-24 18:24:31 +0000

    More formatting fixes

--- split_vcf.py
+++ split_vcf.py
@@ -13,7 +13,6 @@ def split_vcf(vcf_file, output_dir):
         current_count = 0
         in_contact = False
         contact_rows = []  # List to accumulate contact rows
-        
         for row in reader:
             if len(row) > 0:
                 if row[0] == "BEGIN:VCARD":
@@ -23,24 +22,22 @@ def split_vcf(vcf_file, output_dir):
                     in_contact = False
                     contact_rows.append(row)  # Add "END:VCARD" to contact_rows list
                     contact_rows.append([])  # Add a blank line between contacts
-                    current_count += 1
-                    
+                    current_count += 1                 
                     if current_count == contact_number:
                         current_count = 0
-                        write_contact(contact_rows, output_dir, current_file)  # Write the contact rows
+                        write_contact(contact_rows, output_dir, current_file)
                         current_file += 1
                         contact_rows = []  # Reset contact_rows for the next contact
                 else:
                     if in_contact:
                         contact_rows.append(row)  # Add non-empty rows to contact_rows list
-        
-        # Write the last contact after the loop ends
         if contact_rows:
             write_contact(contact_rows, output_dir, current_file)
 
 def write_contact(contact_rows, output_dir, current_file):
     """Writes contacts to output file in the size specified in split_vcf"""
-    with open(os.path.join(output_dir, f"contact_{current_file}.vcf"), "w", encoding="utf-8") as write:
+    with open(os.path.join(output_dir, f"contact_{current_file}.vcf"),
+              "w", encoding="utf-8") as write:
         writer = csv.writer(write, lineterminator='\n')
         for row in contact_rows:
             if row != '':
